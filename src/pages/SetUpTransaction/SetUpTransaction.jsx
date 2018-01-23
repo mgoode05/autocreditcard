4884e67deab33d07b5e0df885845c613772f87e1
Mike Goode <goode.michaelsteven@gmail.com>
2018-01-22 23:59:27 -0800

    Calendar for Out/In renders Date

--- src/pages/SetUpTransaction/SetUpTransaction.jsx
+++ src/pages/SetUpTransaction/SetUpTransaction.jsx
@@ -9,14 +9,34 @@ import 'react-day-picker/lib/style.css';
 class SetUpTransaction extends Component {
     constructor(props) {
         super(props);
-        this.handleDayClick = this.handleDayClick.bind(this);
+        // this.handleDayClick = this.handleDayClickOut.bind(this);
+        // this.handleDayClick = this.handleDayClickIn.bind(this);
         this.state = {
-            selectDay: undefined,
+            selectDayOut: undefined,
+            selectedDayIn: undefined
         };
     }
 
-    handleDayClick(day) {
-        this.setState({ selectedDay: day });
+    handleDayClickOut(day, {selected, disabled}) {
+        if (disabled) {
+            return;
+        }
+        if (selected) {
+            this.setState({ selectedDayOut: undefined});
+            return; 
+        }
+        this.setState({ selectedDayOut: day});
+    };
+
+    handleDayClickIn(day, {selected, disabled}) {
+        if (disabled) {
+            return;
+        }
+        if (selected) {
+            this.setState({ selectedDayIn: undefined});
+            return; 
+        }
+        this.setState({ selectedDayIn: day});
     }
 
     render() {
@@ -25,14 +45,30 @@ class SetUpTransaction extends Component {
     return (
         <div>
                <h3> Create Transaction </h3>
-                (calendar)
-                <DayPicker onDayClick={this.handleDayClick} />
-                {this.state.selectedDay ? (
-                    <p> Your Transaction will occur on {this.state.selectedDay.toLocaleDateString()}</p>
-                ) : (
-                    <p> Please Select A Day</p>
-                )
-            }
+                
+                <DayPicker 
+                    onDayClick={this.handleDayClickOut} 
+                    selectedDays={this.state.selectedDayOut}
+                    dsiabledDays={{ daysOfWeek: [0] }}
+                    />
+                        {this.state.selectedDayOut ? (
+                        <p> Your Transaction will occur on {this.state.selectedDayOut.toLocaleDateString()}</p>
+                             ) : (
+                        <p> Please Select A Day</p>
+                            )
+                        }
+                        <br />
+                        <DayPicker 
+                    onDayClick={this.handleDayClickIn} 
+                    selectedDays={this.state.selectedDayIn}
+                    dsiabledDays={{ daysOfWeek: [0] }}
+                    />
+                        {this.state.selectedDayIn ? (
+                        <p> Your Transaction will occur on {this.state.selectedDayIn.toLocaleDateString()}</p>
+                             ) : (
+                        <p> Please Select A Day</p>
+                            )
+                        }
         </div>
     )}
 }
